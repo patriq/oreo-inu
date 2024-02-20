f1c4199ea239e0bc53565e2984acfb4e91c43da6
Andre <patriqdesigns@gmail.com>
2024-02-20 18:55:16 +0100

    Added mage praying if an iron dragon is next to us during walking

--- src/main/kotlin/slayer/dungeon/KourendCatacombs.kt
+++ src/main/kotlin/slayer/dungeon/KourendCatacombs.kt
@@ -1,10 +1,14 @@
 package slayer.dungeon
 
 import api.containsPlayer
+import org.rspeer.commons.logging.Log
+import org.rspeer.game.component.tdi.Prayer
+import org.rspeer.game.component.tdi.Prayers
 import org.rspeer.game.movement.Movement
 import org.rspeer.game.movement.pathfinding.LocalPathfinder
 import org.rspeer.game.position.Position
 import org.rspeer.game.position.area.Area
+import org.rspeer.game.scene.Npcs
 import org.rspeer.game.scene.Players
 import org.rspeer.game.scene.SceneObjects
 import org.rspeer.game.web.Web
@@ -19,7 +23,7 @@ object KourendCatacombs {
         FIRE_GIANTS_WEST(Area.rectangular(1641, 10073, 1613, 10046, 0), Position(1631, 10056, 0)),
         STEEL_DRAGONS_WEST(Area.rectangular(1596, 10065, 1616, 10048, 0), Position(1608, 10054, 0)),
         DUST_DEVILS_EAST(Area.rectangular(1722, 10038, 1708, 10025, 0), Position(1715, 10032, 0)),
-        ABYSSAL_DEMONS_NORTH(Area.rectangular(1667, 10101, 1680, 10083, 0), Position(1673, 10087, 0)),
+        ABYSSAL_DEMONS_NORTH(Area.rectangular(1670, 10100, 1680, 10093, 0), Position(1675, 10097, 0)),
     }
 
     private val INSIDE_DUNGEON_AREA = Area.rectangular(1747, 10112, 1589, 9978, 0)
@@ -32,6 +36,12 @@ object KourendCatacombs {
     fun walk(location: Location) {
         if (location.area.containsPlayer()) {
             return
+        }
+
+        // If we are super close to iron dragons, pray mage
+        if (Npcs.query().names("Iron dragon").within(10).results().isNotEmpty()) {
+            Log.warn("Praying mage!!!")
+            Prayers.toggle(true, Prayer.Modern.PROTECT_FROM_MAGIC)
         }
 
         // Use web walker to walk through the dungeon
